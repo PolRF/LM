0765dc7d4d95680f2c0cfe23e84a66d8a248df28
PolRF <pol@lumo.homes>
2024-07-07 01:27:00 +0200

    fix

--- model.py
+++ model.py
@@ -376,7 +376,7 @@ class DecoderGroupedQueryHeadAttentionAlibi(nn.Module):
         k = k.repeat(1, self.q_kv_proportion, 1, 1)
         v = v.repeat(1, self.q_kv_proportion, 1, 1)
         print(self.alibi_mask.shape)
-        print(self.alibi_mask[:, :, :T, :T])
+        print(self.alibi_mask[:, :, :T, :T].shape)
         output = torch.nn.functional.scaled_dot_product_attention(
             q,
             k,
