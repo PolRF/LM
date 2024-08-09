d8c022f4531ae768146bdb7042568b98cdf6b40d
PolRF <pol@lumo.homes>
2024-08-09 17:25:20 +0200

    fix

--- train.py
+++ train.py
@@ -434,7 +434,7 @@ if __name__ == "__main__":
     os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
     tr_config = TrainConfig(
-        batch_size=8,
+        batch_size=4,
         block_size=1024,
         eval_iters=200,
         init_lr=6e-4,  # for lr decay (TODO need a lower lr????)
@@ -445,7 +445,7 @@ if __name__ == "__main__":
         weight_decay=1e-1,
         device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         # dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16',
-        gradient_accumulation_steps=8,
+        gradient_accumulation_steps=16,
         loading_mode="from_scratch",
         checkpoint_output_dir=BASE_CHECKPOINT_PATH,
         always_save_checkpoint=False,
